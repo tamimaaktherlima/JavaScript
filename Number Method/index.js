3ead3c7050efe70b1574bc91932ad023e5131cd9
Tamima Akther Lima <168561062+tamimaaktherlima@users.noreply.github.com>
2025-06-17 00:47:20 +0600

    Update index.js

--- Number Method/index.js
+++ Number Method/index.js
@@ -1,6 +1,23 @@
-// number method --> 
+// number method --> convert one data type to another.
+// here num is a integer numebr data type variable
 var num = 52;
 console.log(typeof(num));
 
+// num convert into String
 num = toString(num);
 console.log(typeof(num));
+
+var num1 = "23.25";
+//num1 = parseInt(num1);
+ num = parseFloat(num1)
+console.log(typeof(num1));
+console.log(num1);
+
+var number = 28.23578;
+console.log(number.toFixed(4)); // to fixed number after the decimal point.
+console.log(number.toPrecision(3)); // total je koyta digit print.
+
+// Number --> String or any data type convert into Number.
+console.log(typeof(Number("15")));
+
+
