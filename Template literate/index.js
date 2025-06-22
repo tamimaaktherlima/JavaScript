c7cb97e34db264e4fedab43d6993aaa67f8dc0ae
Tamima Akther Lima <168561062+tamimaaktherlima@users.noreply.github.com>
2025-06-22 23:12:36 +0600

    Template Literals

---
+++ Template literate/index.js
@@ -0,0 +1,9 @@
+// Template Strings use back-ticks (``) rather than the quotes ("") to define a string
+
+let name = `Tamima Akther Lima`;
+let text = `id: 241-15-564
+    University: Daffodil`;
+ console.log(`Welcome to ${name} ${text}`);
+
+ console.log(`Welcome to ${name}
+     ${text}`);
