0dcc5e1fd478357a3f228a23b0ccb7c1b751b443
Tamima Akther Lima <168561062+tamimaaktherlima@users.noreply.github.com>
2025-06-23 23:51:27 +0600

    if, else if, else

---
+++ Conditional Statement/index.js
@@ -0,0 +1,20 @@
+var num = 5;
+
+// Check Even Odd
+if(num % 2 == 0){
+    console.log("Even\n");
+}
+else{
+    console.log("Odd\n");
+}
+
+// Check Positive or Negative
+if(num > 0){
+    console.log("Positive\n");
+}
+else if(num < 0){
+    console.log("Negative\n");
+}
+else{
+    console.log("Equal to Zero\n");
+}
