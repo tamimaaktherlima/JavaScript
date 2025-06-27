9fa9316614d7ac5ce4897bf5094cc169ae1bc1c9
Tamima Akther Lima <168561062+tamimaaktherlima@users.noreply.github.com>
2025-06-27 23:04:06 +0600

    for loop

---
+++ For Loop/index.js
@@ -0,0 +1,21 @@
+
+// document.write("<h1> Hello Bangladesh! </h1>");
+// document.write("<h1> Hello Bangladesh! </h1>");
+// document.write("<h1> Hello Bangladesh! </h1>");
+// document.write("<h1> Hello Bangladesh! </h1>");
+// document.write("<h1> Hello Bangladesh! </h1>");
+// document.write("<h1> Hello Bangladesh! </h1>");
+
+for(var i = 1; i<=5; i++){
+    document.write("<h1> Hello Bangladesh! </h1>");
+}
+
+// for(var i = 1; i<50; i+=2){
+//     document.write(i + " ");
+// }
+
+for(var i = 10; i >= 1; i--){
+    document.write(" " + i);
+}
+
+document.write("<h1> Ending... </h1>");
