05963e1d8e614c2f8999a306d744879917610418
Tamima Akther Lima <168561062+tamimaaktherlima@users.noreply.github.com>
2025-07-01 22:52:13 +0600

    while, do_while loop

--- While, do_while/index.js
+++ While, do_while/index.js
@@ -1,10 +1,23 @@
 var num = 1;
 var sum = 0;
+// While loop
 while(num <= 10){
     if(num % 2 == 0){
+        document.write(" " + num );
         sum+=num;
     }
     num++;
 }
+document.write("</br>");
+
 // document.write("The Sum is: "+ sum);
-document.write(sum);
+document.write("The Sum is: ",sum);
+document.write("<h1>Ending...</h1>")
+
+// do....while loop
+var n = 1;
+do{
+    document.write("<h3>I Love Bangladesh!</h3>" + "</br>");
+    n++;
+}
+while( n<= 5)
