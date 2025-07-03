dd97b48cbcc7aef06fa2483ff4d90339f50ba2a4
Tamima Akther Lima <168561062+tamimaaktherlima@users.noreply.github.com>
2025-07-03 23:44:28 +0600

    Array in JS

---
+++ Array/index.js
@@ -0,0 +1,21 @@
+var name1, name2, name3;
+name1 = "Lima";
+name2 = "Tamima";
+name3 = "Habib";
+
+console.log(name1);
+console.log(name2);
+console.log(name3);
+
+var name = ["Lima" , "Tamima" , "Ashik" , "Habib"];
+console.log(name.length);
+console.log(name);
+
+// push an elements in the last 
+name.push("Nabila");
+console.log(name.length);
+console.log(name);
+
+name.pop();
+console.log(name.length);
+console.log(name);
