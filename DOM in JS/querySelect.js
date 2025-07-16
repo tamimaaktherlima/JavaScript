a30bb8c275a6d86b3647c45e54a15a5cbc482307
Tamima Akther Lima <168561062+tamimaaktherlima@users.noreply.github.com>
2025-07-16 23:53:34 +0600

    querySelector

---
+++ DOM in JS/querySelect.js
@@ -0,0 +1,10 @@
+// getElementById()
+// getElementsByTagName()
+// getElementsByClassName()
+
+// Use all the above by querySelector() 
+// document.getElementById("para").innerHTML = "Myself Tamima Akther Lima";
+// document.getElementsByClassName("pclass")[0].innerHTML = "Paragraph";
+
+// document.querySelector("#para").innerHTML = "Myself Tamima Akther Lima";
+document.querySelector(".para").innerHTML = "Myself Tamima Akther Lima";
