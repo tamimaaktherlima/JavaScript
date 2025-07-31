f782dfd8d86e46f3d65d6b9e264f62365aec1f10
Tamima Akther Lima <168561062+tamimaaktherlima@users.noreply.github.com>
2025-07-31 23:16:02 +0600

    changing CSS style dynamically

---
+++ OnClick Event/Add CSS Style/script.js
@@ -0,0 +1,18 @@
+var myVar = document.querySelector("#paraID");
+function addStyle(){
+    myVar.classList.add("para-style"); // create custom CSS class which is added or removed.
+}
+
+function removeStyle(){
+    myVar.classList.remove("para-style");
+}
+
+
+var myVar1 = document.querySelector("#paraID2");
+function addStyle1(){
+    myVar1.classList.add("para-style");
+}
+
+function removeStyle1(){
+    myVar1.classList.remove("para-style");
+}
