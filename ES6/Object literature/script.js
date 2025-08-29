c9c24839f5f27034599b1099732132891acd6a43
Tamima Akther Lima <168561062+tamimaaktherlima@users.noreply.github.com>
2025-08-30 01:03:32 +0600

    object Literature

---
+++ ES6/Object literature/script.js
@@ -0,0 +1,31 @@
+// Object Literature.
+
+// function student1(name, age){
+//     return {
+//         // name : name,
+//         // age : age
+//         name,
+//         age 
+//     }
+// }
+// console.log(student1("Lima", 23));
+
+// function Inside object
+
+// let message = {
+    // body : function(){
+    //     return ("Hi I'm body function")
+    // }
+//     body(){
+//         return ("Hi I'm body function")
+//     }
+// }
+// console.log(message.body());
+
+let message = {
+    'body name'(){
+        return ("Hi I'm body name function")
+    }
+}
+
+console.log(message['body name']());
