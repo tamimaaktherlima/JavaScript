9d3d8cbf7735ec5dfd63f601e2f02e7adebfc956
Tamima Akther Lima <168561062+tamimaaktherlima@users.noreply.github.com>
2025-09-04 20:17:35 +0600

    forEach

---
+++ ES6/ForEach/script.js
@@ -0,0 +1,19 @@
+// for vs forEach
+
+// for loop
+// var numbers = [10,20,30,40,50];
+// for(let i = 0; i<numbers.length; i++){
+//     console.log(numbers[i]);
+// }
+
+// forEach
+var numbers = [10,20,30,40,50];
+// numbers.forEach(myfunction)
+
+// function myfunction(x){
+//     console.log(x);
+// }
+
+numbers.forEach(function (x){
+    console.log(x);
+})
