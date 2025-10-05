7c775bff49a35a81c01c43cf2917fecacd2ca13d
Tamima Akther Lima <168561062+tamimaaktherlima@users.noreply.github.com>
2025-10-05 23:59:56 +0600

    Update jquery.js

--- API Calling/XML API format/Jquery API/jquery.js
+++ API Calling/XML API format/Jquery API/jquery.js
@@ -1,2 +1,17 @@
 console.clear();
+
 console.log(window)
+const makeRequest = async(url,method) =>{
+    const result = await $.ajax({
+        url : url,
+        method : method,
+    });
+    return result;
+};
+
+const getData = () =>{
+    makeRequest("https://jsonplaceholder.typicode.com/posts/1","GET").then(
+        (res)=>console.log(res)
+    );
+};
+getData();
