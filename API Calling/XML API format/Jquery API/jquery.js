3262e037ab74fc4ea923ea8ae2f54638959e77eb
Tamima Akther Lima <168561062+tamimaaktherlima@users.noreply.github.com>
2025-10-06 01:20:57 +0600

    Jquery API

--- API Calling/XML API format/Jquery API/jquery.js
+++ API Calling/XML API format/Jquery API/jquery.js
@@ -1,17 +1,37 @@
-console.clear();
+// ajax -> asynchronous javascript and xml
 
+console.clear();
 console.log(window)
-const makeRequest = async(url,method) =>{
-    const result = await $.ajax({
-        url : url,
-        method : method,
-    });
-    return result;
+
+const makeRequest = async(method,url,data) =>{
+    try{
+        const result = await $.ajax({
+            url : url,
+            method : method,
+            data : data,    
+        });
+        return result;
+    }catch(err){
+        console.log(err);
+    }
 };
 
 const getData = () =>{
-    makeRequest("https://jsonplaceholder.typicode.com/posts/1","GET").then(
-        (res)=>console.log(res)
-    );
+    makeRequest("GET",'https://jsonplaceholder.typicode.com/posts/1')
+    .then((res) => console.log(res.data))
+    .catch((err)=>console.log(err))
 };
-getData();
+
+const createData = () =>{
+    makeRequest("POST","https://jsonplaceholder.typicode.com/posts",{
+        body: JSON.stringify({
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+        }),
+    })
+    .then((res) => console.log(res.data))
+    .catch((err)=>console.log(err))
+};
+// getData();
+createData();
